9803d1de8b6edfafbd652fb3323ebbc3db38168d
Nora Silfver <nora.silfver@gmail.com>
2025-12-14 14:22:36 +0100

    feat: integrate Background component into SignIn page for improved aesthetics

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
+import { Background } from "@/components/ui/Background";
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
+      <Background />
       <SignIn />
     </div>
   );
